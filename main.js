c1019964f56bf626b2e5aeb18362e96dc4c310a5
Aditya Saini <131801799+Aditya1Saini@users.noreply.github.com>
2023-11-05 23:57:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var typed =new Typed ('.motion1',
+{
+    strings:["Hi,I'm Aditya Saini",'नमस्ते मैं आदित्य सैनी।'],
+    typeSpeed:80,
+    backSpeed:40,
+    lay:0,
+    loop:true
+}
+);
+var typed =new Typed ('.motion2',
+{
+    strings:['Frontend Developer',' Ethical Hacker'],
+    typeSpeed:80,
+    backSpeed:40,
+    lay:0,
+    loop:true
+}
+);
+/*var typed =new Typed ('.logo',
+{
+    strings:["Aditya's Portfolio",'WELCOME EVERYONE'],
+    typeSpeed:80,
+    backSpeed:40,
+    lay:0,
+    loop:true
+}
+);
+*/
+
